93ebbcf3a0ce7f8f12fb4f94ab84a23e3dd2d72b
Phạm Văn Hoàng <vanhoangit2001@gmail.com>
2024-10-09 11:26:23 +0700

    hotfix: install sharp

--- src/pages/Home/components/FilmSection/lib/types.ts
+++
@@ -1,9 +0,0 @@
-// import { Movie } from "../../../../../Types/Types";
-
-// export interface FilmSectionProps {
-//     title: string;
-//     viewMoreLink: string;
-//     mediaType: string;
-//     data: Movie[]; // Đảm bảo kiểu dữ liệu chính xác
-//     isLoading: boolean; // Thêm trạng thái isLoading
-// }
--- src/pages/Home/lib/types.ts
+++
@@ -1,9 +0,0 @@
-// import { Movie } from "../../../Types/Types";
-
-// export interface SwiperData {
-//     title: string;
-//     data: Movie[];
-//     viewMoreLink: string;
-//     media_type: string;
-//     isLoading: boolean;
-// }
